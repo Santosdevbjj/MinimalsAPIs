5d5853094c69d11304f875d547099722446e2062
Sergio <santossergiorealbjj@outlook.com>
2025-08-28 15:45:34 -0300

    Create ServiceExtensions.cs

---
+++ minimal-api-vehicles/src/MinimalApi.Vehicles/Extensions/ServiceExtensions.cs
@@ -0,0 +1,8 @@
+// Aqui você pode adicionar métodos de extensão para organizar a inicialização (se desejar)
+
+namespace MinimalApi.Vehicles.Extensions;
+
+public static class ServiceExtensions
+{
+    // placeholder
+}
